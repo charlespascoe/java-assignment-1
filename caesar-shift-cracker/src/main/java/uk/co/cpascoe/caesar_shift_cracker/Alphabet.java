5083c79ad263106a023019a522f04f18685f0a04
Charles Pascoe <cpascoe95@gmail.com>
2015-10-20 18:52:37 +0100

    Added comments to Alphabet class

--- caesar-shift-cracker/src/main/java/uk/co/cpascoe/caesar_shift_cracker/Alphabet.java
+++ caesar-shift-cracker/src/main/java/uk/co/cpascoe/caesar_shift_cracker/Alphabet.java
@@ -3,26 +3,50 @@ package uk.co.cpascoe.caesar_shift_cracker;
 
 public abstract class Alphabet {
     private String alphabet;
+
+    /**
+     * Class constructor, with the specifed alphabet
+     *
+     * @param alphabet the string of distinct characters that represents this alphabet
+     */
     protected Alphabet(String alphabet) {
         this.alphabet = alphabet;
     }
 
+    /**
+     * Gets the number of characters of this alphabet
+     */
     public int length() {
         return this.alphabet.length();
     }
 
+    /**
+     * Gets the index of the specified charater within this alphabet, or -1 if not present
+     *
+     * @param c the character to get the index of
+     */
     public int indexOf(char c) {
         return this.alphabet.indexOf((int)c);
     }
 
-    public char getChar(int x) {
-        if (x < 0 || x >= this.alphabet.length()) {
+    /**
+     * Gets the character at the specified index
+     *
+     * @param index the index of the character in this alphabet
+     */
+    public char getChar(int index) {
+        if (index < 0 || index >= this.alphabet.length()) {
             throw new Exception("Array index out of bounds!");
         }
 
-        return this.alphabet[x];
+        return this.alphabet[index];
     }
 
+    /**
+     *  Removes all characters not in this alphabet
+     *
+     *  @param text the input text
+     */
     public String stripToAlphabet(String text) {
         StringBuilder str = new StringBuilder();
 
@@ -35,10 +59,20 @@ public abstract class Alphabet {
         return str.toString();
     }
 
+    /**
+     * Gets the expected probabilities of each character of this alphabet
+     */
     public abstract double[] getExpectedProbabilities();
 
+    /**
+     * Computes the probabilities of each character occurring in the provided text
+     *
+     * @param text the input text
+     */
     public double[] computeProbabilities(String text) {
         text = this.stripToAlphabet(text);
+
+        //TODO: Check that text.length is greater than 0
 
         int[] freqs = new int[this.length()];
 
