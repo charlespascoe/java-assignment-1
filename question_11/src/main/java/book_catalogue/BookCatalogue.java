e046c3938fdfe509481d468312ccb4a3d9ac7cb3
Charles Pascoe <cpascoe95@gmail.com>
2015-11-10 11:41:30 +0000

    Replaced Condition type with Matcher<Book>

--- question_11/src/main/java/book_catalogue/BookCatalogue.java
+++ question_11/src/main/java/book_catalogue/BookCatalogue.java
@@ -3,14 +3,13 @@ package book_catalogue;
 import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
-import book_catalogue.query.Condition;
 
 public class BookCatalogue extends ArrayList<Book> {
     public void sort() {
         Collections.sort(this);
     }
 
-    public List<Book> filter(Condition cond) {
+    public List<Book> filter(Matcher<Book> cond) {
         List<Book> results = new ArrayList<Book>();
 
         for (Book book : this) {
