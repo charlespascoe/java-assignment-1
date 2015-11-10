63f47b1c889d0f247550ea3fa64dbca5737bf2d9
Charles Pascoe <cpascoe95@gmail.com>
2015-11-10 11:41:05 +0000

    Condition now implements Matcher interface

--- question_11/src/main/java/book_catalogue/query/Condition.java
+++ question_11/src/main/java/book_catalogue/query/Condition.java
@@ -1,8 +1,9 @@
 package book_catalogue.query;
 
 import book_catalogue.Book;
+import book_catalogue.Matcher;
 
-public abstract class Condition extends QueryComponent {
+public abstract class Condition extends QueryComponent implements Matcher<Book> {
     public Condition(int startPos, int endPos) {
         super(startPos, endPos);
     }
