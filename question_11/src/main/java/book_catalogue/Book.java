1715c86a7e2f71f9f38ba089840124d6ab4c4440
Charles Pascoe <cpascoe95@gmail.com>
2015-11-17 15:08:27 +0000

    Added Book.Field enum, Book.getField(Field), and Book.compareTo(Book,Field)

--- question_11/src/main/java/book_catalogue/Book.java
+++ question_11/src/main/java/book_catalogue/Book.java
@@ -1,8 +1,43 @@
 package book_catalogue;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Book implements Comparable<Book> {
+    public static enum Field {
+        ID ("id"),
+        TITLE ("title"),
+        AUTHOR_FIRST_NAME ("author.first_name"),
+        AUTHOR_SECOND_NAME ("author.second_name"),
+        PUBLISHER ("publisher"),
+        PUBLICATION_YEAR ("publication_year"),
+        STATUS ("status");
+
+        private final String name;
+
+        private Field(String name) {
+            this.name = name;
+        }
+
+        public String toString() {
+            return this.name;
+        }
+
+        public static Field fromString(String text) {
+            for (Field field : Field.values()) {
+                if (field.toString().equals(text.toLowerCase())) {
+                    return field;
+                }
+            }
+
+            // Using unchecked exception, since this should never occur
+            // as the code should always check to see if the text is
+            // a valid field prior to calling this method
+            throw new Error("Can't find Field " + text + " - make sure you check that it's a valid field first!");
+        }
+    }
+
     private String id;
     private String title;
     private Author[] authors;
@@ -41,12 +76,51 @@ public class Book implements Comparable<Book> {
 
     public BookStatus getStatus() { return this.status; }
 
+    public Object getField(Field field) {
+        switch (field) {
+            case ID:
+                return new Integer(this.id);
+            case TITLE:
+                return this.title;
+            case AUTHOR_FIRST_NAME:
+            case AUTHOR_SECOND_NAME:
+                List<String> names = new ArrayList<String>();
+
+                for (Author a : this.authors) {
+                    if (field == Field.AUTHOR_FIRST_NAME) {
+                        names.add(a.getFirstName());
+                    } else {
+                        names.add(a.getSecondName());
+                    }
+                }
+
+                return names.toArray();
+            case PUBLISHER:
+                return this.publisher;
+            case PUBLICATION_YEAR:
+                return new Integer(this.publicationYear);
+            case STATUS:
+                return this.status.name();
+            default:
+                return null;
+        }
+    }
+
     public int compareTo(Book other) {
         int yearDiff = this.publicationYear - other.getPublicationYear();
 
         if (yearDiff != 0) return yearDiff;
 
         return Utils.compareArrays(this.authors, other.getAuthors());
+    }
+
+    public int compareTo(Book other, Field field) {
+        if (field == Field.AUTHOR_FIRST_NAME || field == Field.AUTHOR_SECOND_NAME) {
+            return Utils.compareArrays((String[])this.getField(field), (String[])other.getField(field));
+        } else {
+            Comparable c = (Comparable)this.getField(field);
+            return c.compareTo(other.getField(field));
+        }
     }
 
     @Override
