fc9a9c8a5179b2e92a675b12c7d2969c4fb5630c
Charles Pascoe <cpascoe95@gmail.com>
2015-11-17 13:15:23 +0000

    Added QueryParsingException.getUserMessage(String) method

--- question_11/src/main/java/book_catalogue/query/QueryParsingException.java
+++ question_11/src/main/java/book_catalogue/query/QueryParsingException.java
@@ -21,5 +21,22 @@ public class QueryParsingException extends Exception {
     public int getEndPosition() {
         return this.endPos;
     }
+
+    public String getUserMessage(String input) {
+        StringBuilder str = new StringBuilder();
+            str.append(input).append("\n");
+
+        for (int i = 0; i < input.length(); i++) {
+            if (i >= this.getStartPosition() && i <= this.getEndPosition()) {
+                str.append("^");
+            } else {
+                str.append(" ");
+            }
+        }
+
+        str.append("\n").append(this.getMessage());
+
+        return str.toString();
+    }
 }
 
