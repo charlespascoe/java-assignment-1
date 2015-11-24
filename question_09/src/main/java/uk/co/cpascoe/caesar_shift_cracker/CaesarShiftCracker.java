a839b0340939639ee16023242f4816a880212913
Charles Pascoe <cpascoe95@gmail.com>
2015-11-24 11:02:21 +0000

    Removed CaesarShiftCracker.decrypt method

--- question_09/src/main/java/uk/co/cpascoe/caesar_shift_cracker/CaesarShiftCracker.java
+++ question_09/src/main/java/uk/co/cpascoe/caesar_shift_cracker/CaesarShiftCracker.java
@@ -4,15 +4,27 @@ import java.lang.Math;
 import java.lang.Double;
 
 public class CaesarShiftCracker {
+    public static class Result {
+        public final int bestShift;
+        public final double bestDelta;
+
+        public Result(int bestShift, int bestDelta) {
+            this.bestShift = bestShift;
+            this.bestDelta = bestDelta;
+        }
+    }
+
     private Alphabet alphabet;
 
     public CaesarShiftCracker(Alphabet alphabet) {
         this.alphabet = alphabet;
     }
 
-    public String crack(String ciphertext) throws Exception {
-        double[] probs = this.alphabet.computeProbabilities(ciphertext);
+    public Result guessKey(String ciphertext) throws Exception {
+        return this.guessKey(this.alphabet.computeProbabilities(ciphertext));
+    }
 
+    public Result guessKey(double[] props) throws Exception {
         int bestShift = 0;
         double bestDelta = Double.MAX_VALUE;
 
@@ -25,22 +37,7 @@ public class CaesarShiftCracker {
             }
         }
 
-        System.out.println(bestShift);
-
-        return this.decrypt(ciphertext, bestShift);
-    }
-
-    public String decrypt(String ciphertext, int shift) throws Exception {
-        StringBuilder str = new StringBuilder();
-
-        int len = this.alphabet.length();
-        int inverseShift = len - shift;
-
-        for (char c : this.alphabet.stripToAlphabet(ciphertext).toCharArray()) {
-            str.append(this.alphabet.getChar((this.alphabet.indexOf(c) + inverseShift) % len));
-        }
-
-        return str.toString();
+        return new CaesarShiftCracker.Result(bestShift, bestDelta);
     }
 
     private double getFrequencyDelta(int displacement, double[] observedProbabilities) throws Exception {
