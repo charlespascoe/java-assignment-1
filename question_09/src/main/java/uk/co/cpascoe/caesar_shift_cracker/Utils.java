020610e299f6ddc1dec6cc4347971b1d7ee26e62
Charles Pascoe <cpascoe95@gmail.com>
2015-12-05 14:32:00 +0000

    Added JavaDoc comments to Utils, added Utils.absoluteModulo(int,int)

--- question_09/src/main/java/uk/co/cpascoe/caesar_shift_cracker/Utils.java
+++ question_09/src/main/java/uk/co/cpascoe/caesar_shift_cracker/Utils.java
@@ -4,16 +4,25 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Utils {
+    /**
+     * Computes the greatest common devisor (GCD) of 'a' and 'b'
+     */
     public static int gcd(int a, int b) {
         if (a == 0) return b;
         if (b == 0) return a;
         return a > b ? Utils.gcd(a % b, b) : Utils.gcd(a, b % a);
     }
 
+    /**
+     * Computes Euler's totient of 'x' (number of numbers modulo 'x' coprime to 'x')
+     */
     public static int eulerTotient(int x) {
         return Utils.getCoprimes(x).size();
     }
 
+    /**
+     * Computes the list of coprimes of x modulo x
+     */
     public static List<Integer> getCoprimes(int x) {
         List<Integer> coprimes = new ArrayList<Integer>();
 
@@ -26,6 +35,9 @@ public class Utils {
         return coprimes;
     }
 
+    /**
+     * Computes the value of (base ^ exponent) mod modulus
+     */
     public static int powMod(int base, int exponent, int modulus) {
         int result = 1;
 
@@ -41,10 +53,16 @@ public class Utils {
         return result;
     }
 
+    /**
+     * Computes the multiplicative inverse of x mod modulus
+     */
     public static int multiplicativeModuloInverse(int x, int modulus) {
         return Utils.powMod(x, Utils.eulerTotient(modulus) - 1, modulus);
     }
 
+    /**
+     * Inserts spaces between groups of letters in the input text
+     */
     public static String addSpaces(String text, int groupSize) {
         StringBuilder str = new StringBuilder();
 
@@ -58,5 +76,14 @@ public class Utils {
         }
 
         return str.toString();
+    }
+
+    /**
+     * Computes the positive solution of x mod modulus
+     */
+    public static int absoluteModulo(int x, int modulus) {
+        // This may look strange, but it guarantees that 0 <= x < modulus
+        // as (in Java) -13 % 10 => -3, when it should be 7
+        return ((x % modulus) + modulus) % modulus;
     }
 }
