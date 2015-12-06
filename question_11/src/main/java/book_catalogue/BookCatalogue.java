92772693f60a94cfd2ecd0bde3fc072b1ef1e44a
Charles Pascoe <cpascoe95@gmail.com>
2015-12-06 19:38:24 +0000

    Added BookCatalogue.load()

--- question_11/src/main/java/book_catalogue/BookCatalogue.java
+++ question_11/src/main/java/book_catalogue/BookCatalogue.java
@@ -3,6 +3,9 @@ package book_catalogue;
 import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
+import java.io.*;
+import com.google.gson.*;
+import com.google.gson.stream.*;
 
 public class BookCatalogue extends ArrayList<Book> {
     public void sort() {
@@ -30,6 +33,26 @@ public class BookCatalogue extends ArrayList<Book> {
         }
 
         return null;
+    }
+
+    public boolean load() {
+        try (JsonReader reader = new JsonReader(new InputStreamReader(this.getClass().getClassLoader().getResourceAsStream("books.json")))) {
+            reader.beginArray();
+
+            Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create();
+
+            while (reader.hasNext()) {
+                this.add((Book)gson.fromJson(reader, Book.class));
+            }
+
+            return true;
+        } catch (IOException ex) {
+            System.err.println("Failed to open data file: " + ex.toString());
+        }
+
+        return false;
     }
 }
 
