eed2a6f9b0bbb6757295dec7eaf6099c486a1907
Charles Pascoe <cpascoe95@gmail.com>
2015-12-09 10:38:39 +0000

    Added more stuff to Program

--- question_02/src/main/java/hotel_reservation/Program.java
+++ question_02/src/main/java/hotel_reservation/Program.java
@@ -7,21 +7,37 @@ public class Program {
     public static void main(String[] args) {
         Hotel fawlteyTowers = new Hotel(Program.createExampleData());
 
-
+        try {
+            System.out.println("Available rooms:");
+            for (Room room : fawlteyTowers.getAvailableRooms(Utils.date("2015-07-17"), Utils.date("2015-07-20"), 1)) {
+                System.out.println(room.toString());
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+        }
     }
 
     private static Room[] createExampleData() {
-        List<Room> rooms = new ArrayList<Room>();
+        try {
+            List<Room> rooms = new ArrayList<Room>();
 
-        Room room1 = new Room(1, 2, 20);
-        room1.bookRoom(Utils.date("2015-07-10"), Utils.date("2015-07-16"), 2);
-        room1.bookRoom(Utils.date("2015-07-21"), Utils.date("2015-07-23"), 1);
-        rooms.add(room1);
+            Room room1 = new Room(1, 2, 20);
+            room1.bookRoom(Utils.date("2015-07-10"), Utils.date("2015-07-16"), 2);
+            room1.bookRoom(Utils.date("2015-07-21"), Utils.date("2015-07-23"), 1);
+            rooms.add(room1);
 
-        Room room2 = new Room(2, 4, 100, "Sea View");
-        room2.bookRoom(Utils.date("2015-07-12"), Utils.date("2015-07-26"), 4);
+            Room room2 = new Room(2, 4, 100, "Sea View");
+            room2.bookRoom(Utils.date("2015-07-12"), Utils.date("2015-07-26"), 4);
+            rooms.add(room2);
 
-        return rooms.toArray(new Room[0]);
+            for (Room room : rooms) {
+                System.out.println(room.toString());
+            }
+
+            return rooms.toArray(new Room[0]);
+        } catch (Exception ex) {
+            throw new Error(ex);
+        }
     }
 }
 
