2de28b6d461a003f4ab3ab2965b4a7bb5cc1e4d1
Charles Pascoe <cpascoe95@gmail.com>
2015-12-09 10:36:43 +0000

    Fixed issue with Utils.DATE_CONVERTER

--- question_02/src/main/java/hotel_reservation/Utils.java
+++ question_02/src/main/java/hotel_reservation/Utils.java
@@ -4,22 +4,23 @@ import java.util.concurrent.TimeUnit;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.text.ParseException;
 
 public class Utils {
-    private static final SimpleDateFormat DATE_CONVERTER;
+    private static final SimpleDateFormat DATE_CONVERTER = new SimpleDateFormat("yyyy-MM-dd");
 
-    static {
-        Utils.DATE_CONVERTER = new SimpleDateFormat("YYYY-MM-dd");
+    public static int daysBetweenDates(Calendar start, Calendar end) {
+        return (int)TimeUnit.DAYS.convert(end.getTimeInMillis() - start.getTimeInMillis(), TimeUnit.MILLISECONDS);
     }
 
-    public static long daysBetweenDates(Calendar start, Calendar end) {
-        return TimeUnit.DAYS.convert(end.getTimeInMillis() - start.getTimeInMillis(), TimeUnit.MILLISECONDS);
-    }
-
-    public static Calendar date(String date) {
+    public static Calendar date(String date) throws ParseException {
         Calendar c = Calendar.getInstance();
         c.setTime(Utils.DATE_CONVERTER.parse(date));
         return c;
+    }
+
+    public static String formatDate(Calendar c) {
+        return Utils.DATE_CONVERTER.format(c.getTime());
     }
 
     public static <T> T[] cloneArray(T[] array) {
