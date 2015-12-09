7de85240445e25625a43169b541a51fda58b39c9
Charles Pascoe <cpascoe95@gmail.com>
2015-12-09 08:17:15 +0000

    Added start of Hotel class

---
+++ question_02/src/main/java/hotel_reservation/Hotel.java
@@ -0,0 +1,25 @@
+package hotel_reservation;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Hotel {
+    private Room[] rooms;
+
+    public Hotel(Room[] rooms) {
+        this.rooms = Utils.cloneArray(rooms);
+    }
+
+    public Room[] getAvailableRooms(Calendar startDate, Calendar endDate, int numberOfPeople) {
+        List<Room> availableRooms = new ArrayList<Room>();
+
+        for (Room room : this.rooms) {
+            if (room.isAvailable(startDate, endDate) && room.getNumberOfBeds() <= numberOfPeople) {
+                availableRooms.add(room);
+            }
+        }
+
+        return availableRooms.toArray(new Room[0]);
+    }
+}
+
