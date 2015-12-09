8a39ddad2a53ea34fbad1776858c437f9fb03cae
Charles Pascoe <cpascoe95@gmail.com>
2015-12-09 08:19:58 +0000

    Started adding dummy data to Program

--- question_02/src/main/java/hotel_reservation/Program.java
+++ question_02/src/main/java/hotel_reservation/Program.java
@@ -1,8 +1,27 @@
 package hotel_reservation;
 
-public class Program {
-    public static class main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Program {
+    public static void main(String[] args) {
+        Hotel fawlteyTowers = new Hotel(Program.createExampleData());
+
+
+    }
+
+    private static Room[] createExampleData() {
+        List<Room> rooms = new ArrayList<Room>();
+
+        Room room1 = new Room(1, 2, 20);
+        room1.bookRoom(Utils.date("2015-07-10"), Utils.date("2015-07-16"), 2);
+        room1.bookRoom(Utils.date("2015-07-21"), Utils.date("2015-07-23"), 1);
+        rooms.add(room1);
+
+        Room room2 = new Room(2, 4, 100, "Sea View");
+        room2.bookRoom(Utils.date("2015-07-12"), Utils.date("2015-07-26"), 4);
+
+        return rooms.toArray(new Room[0]);
     }
 }
 
